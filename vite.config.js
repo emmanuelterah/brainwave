1361128687b153d8695a60c1b89bc341caeb99bc
Emmanuel Terah <emmanuelterah98@gmail.com>
2024-07-23 19:37:08 +0300

    changed config.js file

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 5173, // Default port, change if needed
+    host: '0.0.0.0', // This allows access from the local network
+    open: true // Automatically open the browser on server start
+  }
+});
